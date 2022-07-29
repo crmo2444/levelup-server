415b7125ed3f775413e2192ad3325d74c3917f53
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-07-29 10:08:52 -0500

    initial commit

---
+++ db.sql
@@ -0,0 +1,13 @@
+SELECT * FROM levelupapi_gametype;
+SELECT * FROM auth_user;
+SELECT * FROM authtoken_token;
+SELECT * FROM levelupapi_gamer;
+
+SELECT g.id,
+    g.game_type_id,
+    g.title,
+    g.maker,
+    g.gamer_id,
+    g.number_of_players,
+    g.skill_level
+FROM levelupapi_game g
---
+++ levelupapi/models/event.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Event(models.Model):
+
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    organizers = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
